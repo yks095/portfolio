b771c5f1984d790857067f9495cc31cd6666d39c
mkshin96 <tlsanrhs96@naver.com>
2019-08-09 21:58:48 +0900

    create login test

---
+++ portfolio-backend/src/test/java/com/dblab/login/LoginTest.java
@@ -0,0 +1,62 @@
+package com.dblab.login;
+
+import com.dblab.domain.User;
+import com.dblab.repository.UserRepository;
+import com.dblab.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class LoginTest {
+
+    private MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext context;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private UserService userService;
+    @Before
+    public void setUp(){
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+
+        User user = new User();
+        user.setUsername("testUserName");
+        user.setPassword("testUserPassword");
+        user.setEmail("test@gmail.com");
+        user.setRegisteredDate(LocalDateTime.now());
+
+        userService.passwordEncodeAndSave(user);
+    }
+
+    @Test
+    public void loginTest() throws Exception {
+        //로그인
+        mockMvc.perform(post("/login").param("username", "testUserName").param("password", "testUserPassword")
+                .with(csrf())).andExpect(forwardedUrl("/login/success")).andExpect(authenticated()).andExpect(status().is2xxSuccessful());
+    }
+}

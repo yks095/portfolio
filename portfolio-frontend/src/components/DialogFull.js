7e4e4133eba11955fbedc51f8aabe271cf8a817d
pdh6547 <pdh6547@gmail.com>
2019-09-22 23:47:35 +0900

    UI 변경

---
+++ portfolio-frontend/src/components/DialogFull.js
@@ -0,0 +1,92 @@
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import IconButton from '@material-ui/core/IconButton';
+import Slide from '@material-ui/core/Slide';
+import { withStyles } from '@material-ui/core/styles';
+import CloseIcon from '@material-ui/icons/Close';
+import MenuButton from '@material-ui/icons/Menu';
+import React from 'react';
+
+const styles = (theme) => ({
+    root: {
+        float: 'center',
+        textAlign: 'center',
+        margin: 'auto'
+
+    },
+    dialogButton: {
+        float: 'center',
+        position: 'relative',
+        fontSize: 55,
+        fontWeight: 'bold',
+        margin: '5%',
+        width: '100%'
+    },
+    appBar: {
+        position: 'relative',
+    },
+    title: {
+        marginLeft: theme.spacing(2),
+        flex: 1,
+    },
+    button: {
+        margin: theme.spacing(1),
+        // float: 'right'
+    },
+    input: {
+        display: 'none',
+    }
+});
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
+class DialogFull extends React.Component {
+
+    handleClickOpen = () => {
+        this.setState({
+            open: true
+        })
+    }
+
+    handleClose = () => {
+        this.setState({
+            open: false
+        })
+    };
+
+    state = {
+        open: this.props.open,
+        page: this.props.page
+    };
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <div>
+                <IconButton className={classes.button} onClick={this.handleClickOpen}>
+                    <MenuButton fontSize="large" />
+                </IconButton>
+                <Dialog className={classes.appBar} fullScreen open={this.state.open} onClose={this.handleClose} TransitionComponent={Transition}>
+                    <div>
+                        <IconButton className={classes.button} edge="start" color="inherit" onClick={this.handleClose} aria-label="close">
+                            <CloseIcon fontSize="large" />
+                        </IconButton>
+                    </div>
+                    <div className={classes.root}>
+                        <Button className={classes.dialogButton} onClick={this.props.handleHomeButton}>MAIN</Button>
+                        <br />
+                        <Button className={classes.dialogButton}>마이 페이지</Button>
+                        <br />
+                        <Button className={classes.dialogButton}>회원정보 수정</Button>
+                        <br />
+                        <Button className={classes.dialogButton}>로그아웃</Button>
+                    </div>
+                </Dialog>
+            </div>
+        );
+    }
+}
+
+export default withStyles(styles)(DialogFull);
--- portfolio-frontend/src/pages/Home.js
+++ portfolio-frontend/src/pages/Home.js
@@ -1,13 +1,63 @@
+import { Button } from '@material-ui/core';
+import GridList from '@material-ui/core/GridList';
+import { withStyles } from '@material-ui/styles';
 import React from 'react';
+import './Home.css';
+
+const styles = (theme) => ({
+    root: {
+        marginTop: 30,
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        overflow: 'hidden',
+    },
+    button: {
+        fontSize: '200%',
+        fontWeight: 'bold',
+    },
+    gridList: {
+        width: 550,
+        float: 'center',
+        height: 550,
+        display: 'flex',
+    },
+    gridText: {
+        textAlign: 'center',
+        float: 'center',
+        verticalAlign: 'middle'
+    }
+});
 
 class Home extends React.Component {
     render() {
+        const { classes } = this.props;
         return (
             <div>
-                Main
+                <div className="Title_Text">Hello</div>
+                <div className="Text">
+                    My Pages
+                </div>
+                <div className={classes.root}>
+                    <GridList cellHeight={250} className={classes.gridList} cols={2}>
+                        <Button className={classes.button}>
+                            마이 페이지
+                        </Button>
+                        <Button className={classes.button} onClick = {this.props.handleIntroductionButton}>
+                            자기 소개서
+                        </Button>
+                        <Button className={classes.button}onClick = {this.props.handleProjectsButton}>
+                            프로젝트
+                        </Button>
+                        <Button className={classes.button}>
+                            자격증
+                        </Button>
+                        )}
+                    </GridList>
+                </div>
             </div>
         );
     }
 }
 
-export default Home;
+export default withStyles(styles)(Home);
